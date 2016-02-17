366bd27a00ed47d060a45c4dad2ce4709f6c2f66
cupidon4uk <mylyanyk.ivan@gmail.com>
2016-02-18 00:41:44 +0200

    added login with linkedin

--- src.es5.js
+++ src.es5.js
@@ -4,6 +4,25 @@ var config = {
 	divider: '\n---'
 };
 
+function onLinkedInLoad() {
+	IN.Event.on(IN, "auth", getProfileData);
+}
+
+// Handle the successful return from the API call
+function onSuccess(data) {
+	console.log(data);
+}
+
+// Handle an error response from the API call
+function onError(error) {
+	console.log(error);
+}
+
+// Use the API call wrapper to request the member's basic profile data
+function getProfileData() {
+	IN.API.Raw("/people/~").result(onSuccess).error(onError);
+}
+
 var reducers = [[function avatar(u) {
 	return u.avatar ? ' ![avatar][]' : '';
 }, function fullName(u) {
